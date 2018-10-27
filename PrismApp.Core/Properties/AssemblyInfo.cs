2b7be21e96a820b2f8d68929654ccaee7a578608
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2018-10-27 19:21:50 +0200

    dev

--- PrismApp.Core/Properties/AssemblyInfo.cs
+++ PrismApp.Core/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -27,10 +28,12 @@ using System.Runtime.InteropServices;
 //      Major Version
 //      Minor Version
 //      Build Number
-//      Revision
+//      Revis😊ion
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: XmlnsDefinition("http://jtjt.pl/Core", "PrismApp.Core")]
